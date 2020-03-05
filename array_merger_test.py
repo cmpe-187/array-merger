e6a043ee534c975e3e720c37360ad048db32364d
Salleek <sbstevencai@gmail.com>
2020-03-05 12:43:38 -0800

    Created test case for array_merger function.

--- array_merger_test.py
+++ array_merger_test.py
@@ -1,3 +1,14 @@
 __author__ = "Zelin Cai, Patrick Silvestre"
 __version__ = "0.1.0"
 __license__ = "MIT"
+
+from array_merger import *
+import unittest
+
+class test(unittest.TestCase):
+    def test1(self):
+        list1 = [1, 2, 7, 11]
+        list2 = [3, 7, 13, 16, 29]
+        merged_list = array_merger(list1, list2)
+        expected_list = [1, 2, 3, 7, 7, 11, 13, 16, 29]
+        self.assertEqual(merged_list, expected_list)
