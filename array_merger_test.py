53bf40a75a8a239226e92cf05cb4f129a80fc882
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-03-06 00:39:10 -0800

    Add non-empty list tests

--- array_merger_test.py
+++ array_merger_test.py
@@ -29,3 +29,26 @@ class TestOneEmptyList(unittest.TestCase):
         expected_output = [0]
         actual_output = array_merger(list1, list2)
         self.assertEqual(expected_output, actual_output)
+
+
+class TestFullLists(unittest.TestCase):
+    def test_list1_with_leftover_values(self):
+        list1 = [3, 4, 5, 6]
+        list2 = [0, 1, 2]
+        expected_output = [0, 1, 2, 3, 4, 5, 6]
+        actual_output = array_merger(list1, list2)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_list2_with_leftover_values(self):
+        list1 = [0, 1, 2]
+        list2 = [3, 4, 5, 6]
+        expected_output = array_merger(list1, list2)
+        actual_output = array_merger(list1, list2)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_same_sized_lists(self):
+        list1 = [0, 2, 4]
+        list2 = [1, 3, 5]
+        expected_output = [0, 1, 2, 3, 4, 5]
+        actual_output = array_merger(list1, list2)
+        self.assertEqual(expected_output, actual_output)
