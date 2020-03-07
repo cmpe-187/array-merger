be9ff44fdb3e2c637127d746da341a009495ec5f
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-03-07 01:15:53 -0800

    Add nodes visited

--- array_merger_test.py
+++ array_merger_test.py
@@ -7,7 +7,11 @@ import unittest
 
 
 class TestTwoEmptyLists(unittest.TestCase):
-    def test_two_empty_lists(self):
+    def test_01_two_empty_lists(self):
+        """
+        Nodes:
+        1-2-3-8-10-12
+        """
         list1 = []
         list2 = []
         expected_output = []
@@ -16,14 +20,26 @@ class TestTwoEmptyLists(unittest.TestCase):
 
 
 class TestOneEmptyList(unittest.TestCase):
-    def test_first_list_empty(self):
+    def test_02_first_list_empty(self):
+        """
+        Nodes:
+        1-2-3-8-
+            10-11-
+            10-12
+        """
         list1 = []
         list2 = [0]
         expected_output = [0]
         actual_output = array_merger(list1, list2)
         self.assertEqual(expected_output, actual_output)
 
-    def test_second_list_empty(self):
+    def test_03_second_list_empty(self):
+        """
+        Nodes:
+        1-2-3-
+            8-9-
+            8-10-12
+        """
         list1 = [0]
         list2 = []
         expected_output = [0]
@@ -32,21 +48,56 @@ class TestOneEmptyList(unittest.TestCase):
 
 
 class TestFullLists(unittest.TestCase):
-    def test_list1_with_leftover_values(self):
+    def test_04_list1_with_leftover_values(self):
+        """
+        Nodes:
+        1-2-
+            3-4-6-7-
+            3-4-6-7-
+            3-4-6-7-
+            3-
+                8-9-
+                8-9-
+                8-9-
+                8-9-
+                8-10-12
+        """
         list1 = [3, 4, 5, 6]
         list2 = [0, 1, 2]
         expected_output = [0, 1, 2, 3, 4, 5, 6]
         actual_output = array_merger(list1, list2)
         self.assertEqual(expected_output, actual_output)
 
-    def test_list2_with_leftover_values(self):
+    def test_05_list2_with_leftover_values(self):
+        """
+        Nodes:
+        1-2-
+            3-4-5-7-
+            3-4-5-7-
+            3-4-5-7-
+            3-8-
+                10-11-
+                10-11-
+                10-11-
+                10-11-
+                10-12
+        """
         list1 = [0, 1, 2]
         list2 = [3, 4, 5, 6]
         expected_output = array_merger(list1, list2)
         actual_output = array_merger(list1, list2)
         self.assertEqual(expected_output, actual_output)
 
-    def test_same_sized_lists(self):
+    def test_06_same_sized_lists(self):
+        """
+        Nodes:
+        1-2-3-4-5-7-
+            3-4-6-7-
+            3-4-5-7-
+            3-4-6-7-
+            3-4-5-7-
+            3-4-6-7-8-10-12
+        """
         list1 = [0, 2, 4]
         list2 = [1, 3, 5]
         expected_output = [0, 1, 2, 3, 4, 5]
